5d541a7e070e864eb40970d6cbdc9ed1df0ad7f9
Ilya Alexeev <devtective.exe@gmail.com>
2025-06-26 20:49:36 +0300

    fix: correct import path for SearchForm component and add TaskBoard  in the template

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
-import SearchForm from './components/search-form/search-from.vue'
-
 <script setup lang="ts">
 import SearchForm from './components/search-form/search-form.vue';
-
+import TaskBoard from './components/task-board/task-board.vue';
 </script>
+
 <template>
     <SearchForm/>
+    <TaskBoard/>
 </template>
 
