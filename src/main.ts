f3fbe01b46b0ca638749a62121368e9fdc941162
Ilya Alexeev <devtective.exe@gmail.com>
2025-06-26 18:52:57 +0300

    feat: add  task-item styles import in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './styles/style.sass'
 import './styles/search-form.sass'
+import './styles/task-item.sass'
 import App from './App.vue'
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
@@ -9,6 +10,5 @@ import { toastOptions } from './utils/toast-config'
 
 
 const app = createApp(App);
-
 app.use(Toast, toastOptions);
 app.mount('#app');
