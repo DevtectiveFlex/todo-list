f79a5cc5bb14db92e768093765d9b322bc4bd987
Ilya Alexeev <devtective.exe@gmail.com>
2025-06-26 20:43:46 +0300

    feat: integrate Pinia for state management in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { createApp } from 'vue'
 import './styles/style.sass'
 import './styles/search-form.sass'
 import './styles/task-item.sass'
+import './styles/task-board.sass'
 import App from './App.vue'
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 import { toastOptions } from './utils/toast-config'
+import { createPinia } from 'pinia'
 
 
-
+const pinia = createPinia()
 const app = createApp(App);
 app.use(Toast, toastOptions);
+app.use(pinia);
 app.mount('#app');
