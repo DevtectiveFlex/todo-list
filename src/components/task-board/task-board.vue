bdc202f20b5866d01f1a22400f418fc571fc7ab0
Ilya Alexeev <devtective.exe@gmail.com>
2025-06-27 05:24:03 +0300

    refactor: enhance accessibility attributes and simplify TaskItem binding

--- src/components/task-board/task-board.vue
+++ src/components/task-board/task-board.vue
@@ -8,20 +8,20 @@ const taskStore = useTaskStore();
 <template>
   <section class="taskboard">
     <div class="taskboard__columns">
-      <span class="taskboard__column"></span>
-      <span class="taskboard__column taskboard__column_title">Описание</span>
-      <span class="taskboard__column taskboard__column_status">Статус</span>
-      <span class="taskboard__column taskboard__column_date">Дата</span>
+      <span class="taskboard__column" aria-hidden="true"></span>
+      <span class="taskboard__column taskboard__column_title" aria-label="Колонка 'Описание задачи'">Описание</span>
+      <span class="taskboard__column taskboard__column_status" aria-label="Колонка 'Статус задачи'">Статус</span>
+      <span class="taskboard__column taskboard__column_date" aria-label="Колонка 'Дата'">Дата</span>
     </div>
     <ul class="task-list">
       <TaskItem
         v-for="task in taskStore.visibleTasks"
         :key="task.id"
-        :id="task.id"
-        :title="task.title"
-        :completed="task.completed"
-        :date="task.date"
+        v-bind="task"
       />
+      <li v-if="!taskStore.visibleTasks.length">
+        Кажется, здесь должны были быть задачи...
+      </li>
     </ul>
   </section>
 </template>
